87928e95e391be61e08eec5133982591734e805a
Big Dog Dean <diazdean21@gmail.com>
2022-02-06 14:48:08 -0800

    completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Article from "./Article";
+
+
+function ArticleList({ articles }) {
+    const ArticleItem = articles.map((article) => {
+        return ( 
+            <Article key = {article.id} title = {article.title} date = {article.date} preview ={article.preview} />
+        )
+    })
+
+    return(
+        <main>
+            {ArticleItem}
+        </main>
+
+    )
+    }
+
+export default ArticleList; 
